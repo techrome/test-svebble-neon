691906d780afcc7b43a504c067f908538fa379bf
Alexander Katchkatchishvili <alexander.katchkatchishvili@flatrocktech.com>
2025-11-09 19:06:26 +0400

    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import { getDBURL } from "@/db/helpers/getDBURL";
-// test
+
 export const db = process.env.DATABASE_URL
   ? drizzleNeon(neon(process.env.DATABASE_URL))
   : drizzlePg({
