35e8770809e236444dce572f39e5a35e36cc18a5
Alexander Katchkatchishvili <alexander.katchkatchishvili@flatrocktech.com>
2025-11-10 23:30:01 +0400

    mui + tailwind fixes (not done)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,45 @@
 import type { AppType } from "next/app";
 import Head from "next/head";
-
+import {
+  AppCacheProvider,
+  createEmotionCache,
+} from "@mui/material-nextjs/v16-pagesRouter";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Roboto } from "next/font/google";
 import { trpc } from "@/trpc/client";
 
 import "@/styles/global.css";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 
-const App: AppType = ({ Component, pageProps }) => {
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "var(--font-roboto)",
+  },
+  cssVariables: true,
+});
+
+const clientCache = createEmotionCache({ enableCssLayer: true, key: "my-mui" });
+
+const App: AppType = ({ Component, pageProps, emotionCache = clientCache }) => {
   return (
-    <>
+    <AppCacheProvider emotionCache={emotionCache}>
+      {/* <GlobalStyles styles="@layer theme, base, mui, components, utilities;" /> */}
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <Component {...pageProps} />
-    </>
+      <ThemeProvider theme={theme}>
+        <main className={roboto.variable}>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </AppCacheProvider>
   );
 };
 
