a4e68dd42a839a952f42933019fcdb77f8ad290b
Alexander Katchkatchishvili <alexander.katchkatchishvili@flatrocktech.com>
2025-11-11 11:09:45 +0400

    fix: properly fixed mui and tailwind layers

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,37 +1,15 @@
-import {
-  Head,
-  Html,
-  Main,
-  NextScript,
-  DocumentProps,
-  DocumentContext,
-} from "next/document";
-import {
-  DocumentHeadTags,
-  documentGetInitialProps,
-  DocumentHeadTagsProps,
-} from "@mui/material-nextjs/v16-pagesRouter";
-import { createEmotionCache } from "@/utils/createEmotionCache";
+import { Head, Html, Main, NextScript } from "next/document";
 
-const Document = (props: DocumentProps & DocumentHeadTagsProps) => {
+const Document = () => {
   return (
     <Html>
-      <Head>
-        <DocumentHeadTags {...props} />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-};
-
-Document.getInitialProps = async (ctx: DocumentContext) => {
-  const finalProps = await documentGetInitialProps(ctx, {
-    emotionCache: createEmotionCache(),
-  });
-  return finalProps;
 };
 
 export default Document;
