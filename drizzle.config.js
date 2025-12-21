5d1a3dd37c2c24ff69df0cc3f1065173aa681d8a
Alexander Katchkatchishvili <alexander.katchkatchishvili@flatrocktech.com>
2025-12-21 23:52:27 +0400

    fix: drizzle-kit migration import fix

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,7 +5,7 @@ import { getDBURLPrimary } from "./scripts/helpers/getDBURL.mjs";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: getDBURLPrimary(),
--- scripts/migrate-prod.mjs
+++ scripts/migrate-prod.mjs
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
+
 import { getDBURLPrimary } from "./helpers/getDBURL.mjs";
 
 const db = drizzle(neon(getDBURLPrimary()));
