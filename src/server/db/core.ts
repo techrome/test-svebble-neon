6987acff182f694a6dfb097d3d8c83e363565eee
techrome <alextechstart@gmail.com>
2025-12-24 22:20:20 +0400

    test

--- src/server/db/core.ts
+++ src/server/db/core.ts
@@ -3,7 +3,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 
-// relative paths because this file is used in some cli and they don't support TS path aliases
+// relative paths here because this file is used in some cli and they don't support TS path aliases
 import { getDBURL } from "../../../scripts/helpers/getDBURL";
 import { env } from "../env";
 
