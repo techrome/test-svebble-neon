1dc18ceb2bd241f08f6cf63749449c6ead6c8f19
techrome <58153395+techrome@users.noreply.github.com>
2025-12-28 07:05:01 +0400

    feat: global error handling for mutations, loadingBoundary tweak

--- src/components/LoadingBoundary/LoadingBoundary.tsx
+++ src/components/LoadingBoundary/LoadingBoundary.tsx
@@ -6,13 +6,24 @@ import {
 } from "@/utils/loadingBoundaryContext";
 import AnimationWrapper from "./AnimationWrapper";
 
-const LoadingBoundary = ({ children }: { children: React.ReactNode }) => {
+type Props = {
+  children: React.ReactNode;
+  alwaysActive?: boolean;
+  isOuter?: boolean;
+};
+
+const LoadingBoundary = ({ children, alwaysActive, isOuter }: Props) => {
   const [queryKeys, setQueryKeys] = React.useState<QueryKeys>({});
   const hasActiveQueries = Object.keys(queryKeys).length > 0;
 
   return (
     <LoadingBoundaryContext.Provider value={{ queryKeys, setQueryKeys }}>
-      <AnimationWrapper active={hasActiveQueries}>{children}</AnimationWrapper>
+      <AnimationWrapper
+        active={alwaysActive || hasActiveQueries}
+        isOuter={isOuter}
+      >
+        {children}
+      </AnimationWrapper>
     </LoadingBoundaryContext.Provider>
   );
 };
