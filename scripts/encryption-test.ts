4568cb7c76dfc37a28082976bb14c9c9a1c002a2
techrome <58153395+techrome@users.noreply.github.com>
2026-03-09 10:41:11 +0400

    fix: slight fixes for encryption test script

---
+++ scripts/encryption-test.ts
@@ -0,0 +1,41 @@
+import sodium from "libsodium-wrappers";
+import { db } from "../src/server/db";
+import { audit_log } from "../src/server/db/schema";
+
+const publicKey = Buffer.from("test1", "base64url");
+const privateKey = Buffer.from("test2", "base64url");
+
+async function main() {
+  await sodium.ready;
+
+  // const encrypted = sodium.crypto_box_seal(
+  //   Buffer.from("test", "utf-8"),
+  //   publicKey
+  // );
+
+  // console.log("Encrypyed text: ", Buffer.from(encrypted).toString("base64url"));
+  const auditLogRows = await db
+    .select({ ip_address: audit_log.ip_address })
+    .from(audit_log);
+  for (const row of auditLogRows) {
+    try {
+      if (row.ip_address) {
+        const decrypted = sodium.crypto_box_seal_open(
+          Buffer.from(row.ip_address, "base64url"),
+          publicKey,
+          privateKey
+        );
+
+        console.log(
+          "Decrypted text: ",
+          Buffer.from(decrypted).toString("utf-8")
+        );
+      }
+    } catch (_err) {}
+  }
+}
+
+main().catch((err) => {
+  console.error("Failed to encrypt", err);
+  process.exit(1);
+});
