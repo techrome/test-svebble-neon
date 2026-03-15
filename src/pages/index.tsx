b1f36ef6bff59cde780c9ff1cb4458080f1e5b2a
techrome <58153395+techrome@users.noreply.github.com>
2026-03-15 16:01:28 +0400

    fix: unused imports for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,68 +2,17 @@ import React from "react";
 import { type GetStaticProps } from "next";
 import Button from "@/components/Button/Button";
 import clsx from "clsx";
-import ViewListIcon from "@mui/icons-material/ViewList";
-import PersonIcon from "@mui/icons-material/Person";
 import { Paper, Typography } from "@mui/material";
-import SendIcon from "@mui/icons-material/Send";
-import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import { Virtuoso, type VirtuosoHandle } from "react-virtuoso";
-import ReplayIcon from "@mui/icons-material/Replay";
-import debounce from "lodash/debounce";
-import WavingHandIcon from "@mui/icons-material/WavingHand";
 
 import { utils as serverUtils } from "@/server";
-import { type RouterInput, trpc } from "@/trpc";
-import useAppQuery from "@/utils/hooks/useAppQuery";
-import { Comment } from "@/components/CommentsList/CommentsList";
-import LoadingBoundary from "@/components/LoadingBoundary/LoadingBoundary";
-import {
-  useGlobalDrawer,
-  useGlobalModal,
-  useLocalDrawer,
-  useLocalModal,
-} from "@/utils/hooks/useOverlay";
+import { trpc } from "@/trpc";
 import {
   defaultPadding,
   HorizontalStack,
-  Section,
   VerticalStack,
 } from "@/components/Layout/Containers";
-import { useAppSnackbar } from "@/utils/snackbar";
-import {
-  CACHE_TIME_MS,
-  CACHE_TIME_S,
-  minutes,
-  seconds,
-} from "@/utils/cacheTime";
-import IconButton from "@/components/Button/IconButton";
-import Tooltip from "@/components/Tooltip/Tooltip";
-import { SectionWrapper } from "@/pages/app/my-profile";
-import { Divider } from "@/components/Layout/Dividers";
-import { userLoginLifecycle } from "@/trpc/helpers/userLifecycle";
-import { InfiniteData, useQueryClient } from "@tanstack/react-query";
-import z from "zod";
-import { Text } from "@/utils/validators/helpers/text";
-import { useUser } from "@/trpc/hooks/useUser";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import Input from "@/components/Fields/Input";
+import { CACHE_TIME_MS, CACHE_TIME_S } from "@/utils/cacheTime";
 import { type AppPage } from "@/pages/_app";
-import { useAuthModal } from "@/utils/hooks/useAuthModal";
-import {
-  type MessageCreateFormValues,
-  makeMessageCreateSchemaForm,
-} from "@/utils/validators/shared/messages";
-import { useRouter } from "next/router";
-import { getRouterQueryValue } from "@/utils/query";
-import { useWebsockets } from "@/trpc/hooks/useWebsockets";
-import {
-  getChannelId,
-  subscribeWs,
-  WebsocketEventName,
-} from "@/trpc/helpers/websockets";
-import { getQueryKey } from "@trpc/react-query";
 import ChannelList from "@/components/Chat/ChannelList";
 import { APP_NAME } from "@/utils/constants";
 import Link from "@/components/Link/Link";
