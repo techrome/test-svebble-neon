da2d8b1fabc33a21dca1e78b600f4c60450d94e4
techrome <58153395+techrome@users.noreply.github.com>
2026-05-13 06:27:00 +0400

    chore

--- scripts/encryption-test.ts
+++ scripts/encryption-test.ts
@@ -1,6 +1,7 @@
 import sodium from "libsodium-wrappers";
 import { db } from "../src/server/db";
 import { audit_log } from "../src/server/db/schema";
+// import f from "./audit_log.json";
 
 const publicKey = Buffer.from("test1", "base64url");
 const privateKey = Buffer.from("test2", "base64url");
@@ -14,25 +15,28 @@ async function main() {
   // );
 
   // console.log("Encrypyed text: ", Buffer.from(encrypted).toString("base64url"));
-  const auditLogRows = await db
-    .select({ ip_address: audit_log.ip_address })
-    .from(audit_log);
-  for (const row of auditLogRows) {
-    try {
-      if (row.ip_address) {
-        const decrypted = sodium.crypto_box_seal_open(
-          Buffer.from(row.ip_address, "base64url"),
-          publicKey,
-          privateKey
-        );
-
-        console.log(
-          "Decrypted text: ",
-          Buffer.from(decrypted).toString("utf-8")
-        );
-      }
-    } catch (_err) {}
-  }
+  // const auditLogRows = await db
+  //   .select({ ip_address: audit_log.ip_address })
+  //   .from(audit_log);
+  let obj: Record<string, number> = {};
+  // for (const row of f) {
+  //   try {
+  //     if (row.ip_address) {
+  //       const decrypted = sodium.crypto_box_seal_open(
+  //         Buffer.from(row.ip_address, "base64url"),
+  //         publicKey,
+  //         privateKey
+  //       );
+  //       const ip_addr = Buffer.from(decrypted).toString("utf-8");
+  //       obj[ip_addr] = (obj[ip_addr] || 0) + 1;
+  //       // console.log(
+  //       //   "Decrypted text: ",
+  //       //   Buffer.from(decrypted).toString("utf-8") + " " + row.created_at
+  //       // );
+  //     }
+  //   } catch (_err) {}
+  // }
+  console.log({ obj });
 }
 
 main()
