03e0c978e99f549c401f3698e495859edcfb210a
techrome <58153395+techrome@users.noreply.github.com>
2026-07-14 06:10:37 +0400

    db: seeded default reactions

---
+++ drizzle/0033_seed-default-reactions.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    "reactions" ("kind", "slug", "emoji", "sort_order")
+VALUES
+    ('unicode', 'thumbs-up', '👍', 100),
+    ('unicode', 'heart', '❤️', 200),
+    ('unicode', 'laugh', '😂', 300),
+    ('unicode', 'surprised', '😮', 400),
+    ('unicode', 'sad', '😢', 500),
+    ('unicode', 'party-popper', '🎉', 600),
+    ('unicode', 'fire', '🔥', 700),
+    ('unicode', 'thumbs-down', '👎', 800);
